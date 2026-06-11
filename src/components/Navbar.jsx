ae4aefb94b863f595e4cb7e9a36a1e9429463b24
= <techapoorv41@gmail.com>
2026-06-11 14:46:16 +0530

    Day1 :Project set with Tailwind and Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold text-green-400">Dev Tracker</h1>
+      <div className="flex gap-6">
+        <a href="#" className="hover:text-green-400">Github</a>
+        <a href="#" className="hover:text-green-400">Leetcode</a>
+        <a href="#" className="hover:text-green-400">Compare</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
